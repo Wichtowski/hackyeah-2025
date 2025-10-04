481192e9a6c7b5c3e525ff7662146678574a28da
oskar-wichtowski-wttech <oskar.wichtowski@wundermanthompson.com>
2025-10-04 21:32:31 +0200

    Add Favourites tab and screen: Introduce a new 'Favourites' tab in TabLayout and create FavouritesScreen component with Header integration. Update icon mapping for 'heart.fill'.

---
+++ journey-radar/app/(tabs)/favourites.tsx
@@ -0,0 +1,8 @@
+import { Header } from "@/components/header";
+export default function FavouritesScreen() {
+  return (
+    <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
+      <Header />
+    </div>
+  );
+}
